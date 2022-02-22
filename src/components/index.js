34c0ae38bda77ee29a66ab850236943d3f88161c
Bob McAleavey <bobmcaleavey@gmail.com>
2022-02-22 17:50:45 -0500

    add OverflowContainer component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export {default as OverflowContainer } from './OverflowContainer.svelte'
 export { default as Table } from './Table.svelte';
 export { default as Th } from './Th.svelte';
 export { default as Thead } from './Thead.svelte';
