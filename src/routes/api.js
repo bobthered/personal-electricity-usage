7e86327766646aa2592e232a55dab97b2b51635c
Bob McAleavey <bobmcaleavey@gmail.com>
2022-02-25 13:57:43 -0500

    add post endpoint

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,21 @@
 import { clientPromise } from '@lib/mongodb';
 
-export async function get () {
+export const get = async () => {
   const connection = await clientPromise;
   const docs = await connection.db().collection('data').find().toArray();
   return {
     status: 200,
-    body : {
+    body: {
       docs
     }
+  }
+}
+export const post = async ({ request }) => {
+  const body = await request.json();
+  const connection = await clientPromise;
+  await connection.db().collection('data').insertOne(body);
+  return {
+    status: 200,
+    body: {}
   }
 }
