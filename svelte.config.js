b5c97b882d129f497db089e2ca9dd48f5e4c2e6f
Bob McAleavey <bobmcaleavey@gmail.com>
2022-02-28 16:48:32 -0500

    add adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,24 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			env: {
+				port: process.env.PORT | 3000,
+				host: '0.0.0.0'
+			}
+		}),
 		vite: {
 			resolve: {
 				alias: {
-					'@actions' : path.resolve('./src/actions'),
-					'@components' : path.resolve('./src/components'),
-					'@lib' : path.resolve('./src/lib'),
-					'@routes' : path.resolve('./src/routes'),
-					'@stores' : path.resolve('./src/stores'),
+					'@actions': path.resolve('./src/actions'),
+					'@components': path.resolve('./src/components'),
+					'@lib': path.resolve('./src/lib'),
+					'@routes': path.resolve('./src/routes'),
+					'@stores': path.resolve('./src/stores'),
 				}
 			}
 		}
